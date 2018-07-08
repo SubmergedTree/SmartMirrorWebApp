04fefe86a1999dd2f2f2d7bdcd3fb5095ad79267
SubmergedTree <jannmann1@web.de>
2018-07-08 23:00:34 +0200

    framework

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk'
+import {Provider} from 'react-redux'
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import allReducers from './reducer'
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(allReducers, applyMiddleware(thunk));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+    , document.getElementById('root'));
 registerServiceWorker();
