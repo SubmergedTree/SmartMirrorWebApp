ae782a3d3c96dc6394f54340a99a484f2fa85ac3
SubmergedTree <jannmann1@web.de>
2018-07-14 00:35:10 +0200

    header-view event  handler

--- src/container/header-view.js
+++ src/container/header-view.js
@@ -6,13 +6,34 @@ import {bindActionCreators} from 'redux'
 import {mirrorStatus} from '../actions/find-mirror-action'
 
 class HeaderView extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {url: ''}
+
+        this.handleSubmitUrl = this.handleSubmitUrl.bind(this);
+        this.handleUrlChange = this.handleUrlChange.bind(this);
+    }
+
+    handleSubmitUrl(event) {
+        alert(this.state.url)
+        event.preventDefault();
+    }
+
+    handleUrlChange(event) {
+        this.setState({url: event.target.value})
+    }
 
     render() {
         if (!this.props.isMirrorUp) {
             return (
                 <div>
-                <div className="enterSmartMirrorUrl"> Enter SmartMirror URL</div>
-                <button  onClick={() => this.props.mirrorStatus('foo')}>FIND</button>
+                    <form onSubmit={this.handleSubmitUrl}>
+                        <label>
+                        Enter Url:
+                        <input type="text"  value={this.state.url} onChange={this.handleUrlChange}/>
+                        </label>
+                        <input type="submit" value="Submit" />
+                    </form>
                 </div>
             );
         }
@@ -20,7 +41,6 @@ class HeaderView extends Component {
             <div> Mirror found</div>
         );
     }
-
 }
 
 
