b89033d4127138f5577f31760e156933f5a8ef40
SubmergedTree <jannmann1@web.de>
2018-07-15 18:47:14 +0200

    mirrorAccepted selector

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -3,3 +3,7 @@ export const getMirrorStatus = state => state.mirrorStatus.mirrorFound
 
 export const getUrl = state => state.urlReducer.url
 
+export const mirrorAccepted = state => {
+    const url = state.urlReducer.url;
+    return url.length !== 0;
+};
