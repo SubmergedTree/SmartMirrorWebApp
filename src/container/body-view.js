d4365abe915517374d9af426c0f90eb79152ef50
SubmergedTree <jannmann1@web.de>
2018-07-18 22:20:47 +0200

    draw received users

--- src/container/body-view.js
+++ src/container/body-view.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux'
 import {getUrl, mirrorAccepted} from '../selectors/index' 
-import {SelectUserView} from './select-user-view'
+import SelectUserView from './select-user-view'
 
 class BodyView extends Component {
     render() {
--- src/container/select-user-view.js
+++ src/container/select-user-view.js
@@ -1,9 +1,39 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux'
+
+import { getUsers as getUsersSelector, getUrl} from '../selectors/index' 
+import { getUsers as getUsersAction } from '../actions/user-actions'
 
 
-export class SelectUserView extends Component {
+class SelectUserView extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            
+        };
+    }
+
+    componentDidMount() {
+        this.props.getUsersAction(this.props.url);
+    }
+
     render() {
+        const users = this.props.users;
+
         return(
+            <div className="centerizedHeading">
+                Select User
+               <ul>
+                    {
+                        users.map((item, key) => <li key={key}> <a>{item.username}</a></li>)
+                    }
+                </ul>
+            </div>
+        );
+
+     /*   return(
             <div className="centerizedHeading">
                 Select User
                 <ul>
@@ -13,7 +43,21 @@ export class SelectUserView extends Component {
                 <li><a> About</a></li>
                 </ul>
             </div>
-        );
+        );*/
     }
 }
 
+function mapStateToProps(state) {
+    return {
+        users: getUsersSelector(state),
+        url: getUrl(state)
+    };
+}
+
+function dispatchInput(dispatch) {
+    return bindActionCreators({
+        getUsersAction: getUsersAction
+    }, dispatch);
+}
+
+export default connect(mapStateToProps, dispatchInput)(SelectUserView)
