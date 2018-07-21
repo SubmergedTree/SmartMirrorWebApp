c42dd7626c46a62c1b809c61e955e427ac23aaed
SubmergedTree <jannmann1@web.de>
2018-07-21 21:35:03 +0200

    users are clickable

--- src/container/select-user-view.js
+++ src/container/select-user-view.js
@@ -7,16 +7,14 @@ import { getUsers as getUsersAction } from '../actions/user-actions'
 
 
 class SelectUserView extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            
-        };
-    }
 
     componentDidMount() {
         this.props.getUsersAction(this.props.url);
+    }
+
+    onUserClick(key) {
+        const clickedUser = this.props.users[key];
+        
     }
 
     render() {
@@ -27,7 +25,9 @@ class SelectUserView extends Component {
                 Select User
                <ul>
                     {
-                        users.map((item, key) => <li key={key}> <a>{item.username}</a></li>)
+                        users.map((item, key) => {
+                            return <li key={key} onClick={() => this.onUserClick(key)}> <a>{item.username}</a></li>
+                        })
                     }
                 </ul>
             </div>
