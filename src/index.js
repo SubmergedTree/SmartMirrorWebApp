841b52e992c7ba2651b373e92963b823edd6c2ae
SubmergedTree <jannmann1@web.de>
2018-08-12 17:08:05 +0200

    enable redux dev tools

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk'
 import {Provider} from 'react-redux'
 import App from './components/App';
@@ -8,8 +8,9 @@ import registerServiceWorker from './registerServiceWorker';
 import allReducers from './reducer'
 import './index.css';
 
-
-const store = createStore(allReducers, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // DEV
+const store = createStore(allReducers, /* preloadedState, */ composeEnhancers(applyMiddleware(thunk))); //DEV
+//const store = createStore(allReducers, applyMiddleware(thunk)); PROD
 
 ReactDOM.render(
     <Provider store={store}>
