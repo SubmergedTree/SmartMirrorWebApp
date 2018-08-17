b170d97b4cab372ad6b23ded3eb557378b09f4a3
SubmergedTree <jannmann1@web.de>
2018-08-17 15:38:52 +0200

    file-io-reducer spec

---
+++ src/tests/reducer/file-io-reducer.spec.js
@@ -0,0 +1,49 @@
+import {addImageReducer, fileApiIsSupportedReducer} from '../../reducer/file-io-reducer'
+import {FILEIO} from '../../actions/file-io-action'
+
+describe('fileApiIsSupportedReducer', () => {
+
+    it('should return the initial state ', () => {
+        expect(fileApiIsSupportedReducer(undefined, {})).toEqual(
+            {
+                fileApiSupported: false
+            }
+        )
+    });
+
+    it('should set fileApiSupported to true ', () => {
+        expect(fileApiIsSupportedReducer([], {       
+            type: FILEIO.FILE_IO_SUPPORTED,
+            apiSupported: true
+        })).toEqual(
+            {
+                fileApiSupported: true
+            }
+        )
+    });
+});
+
+
+describe('addImageReducer', () => {
+
+    it('should return the initial state ', () => {
+        expect(addImageReducer(undefined, {})).toEqual(
+            {
+                images: []
+            }
+        )
+    });
+
+    it('should add an image', () => {
+        expect(addImageReducer({
+            images: ["foo"]
+        }, {       
+            type: FILEIO.ADD_IMAGE,
+            image: "bar"
+        })).toEqual(
+            {
+                images: ["foo", "bar"]
+            }
+        )
+    });
+});
