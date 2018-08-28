79dbf053b7e9d33d33dadc6d71f8f4105a3183af
SubmergedTree <jannmann1@web.de>
2018-08-28 20:07:36 +0200

    fix visibility

---
+++ src/actions/update-actions.js
@@ -0,0 +1,56 @@
+import 'isomorphic-fetch';
+import {UrlEndpoints} from '../urlEndpoints'
+
+export const UPDATE_ACTION_TYPES = {
+    IMAGES_TRANSFERRED: 'IMAGES_TRANSFERRED',
+    FAILED_IMAGES_TRANSFER: 'FAILED_IMAGES_TRANSFER',
+    START_SENDING: 'START_SENDING'
+}
+
+function failedToSendImages(e) {
+    console.error(e);
+    return {
+        type: UPDATE_ACTION_TYPES.FAILED_IMAGES_TRANSFER
+    }
+}
+
+function successfulImagesTransfer(body) {
+    console.log(body);
+    return {
+        type: UPDATE_ACTION_TYPES.IMAGES_TRANSFERRED
+    }
+}
+
+function startSending() {
+    return {
+        type: UPDATE_ACTION_TYPES.START_SENDING
+    }
+}
+
+export function sendImagesToMirror(images, url) {
+    url = url + UrlEndpoints.addPictures;
+
+    const formData = new FormData();
+    formData.append('numberOf', images.length);
+    formData.append('username', 'foo');
+    for (let imageNum = 0; imageNum < images.length; imageNum++) {
+        const nameNum = (imageNum + 1).toString();
+        formData.append(nameNum, images[imageNum], nameNum);
+    }
+
+    return dispatch => {
+        try {
+            dispatch(startSending())
+            return fetch(url, {
+                method: 'POST',
+                credentials: 'same-origin',
+                body: formData
+            })
+            .then(res => res.json())
+            .then(res => dispatch(successfulImagesTransfer(res)))
+            .catch(ex => dispatch(failedToSendImages(ex)))
+        } catch(e) {
+            dispatch(failedToSendImages(e)); 
+        }
+      }
+}
