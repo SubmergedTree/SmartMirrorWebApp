6f17f633469ca2123c7cfb34163d9bea9878c044
SubmergedTree <jannmann1@web.de>
2018-09-09 15:29:03 +0200

    class to className

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function Footer(props) {
     return (
-    <div class="footer">
+    <div className="footer">
         <h3>Github</h3>
         <div><a href="https://github.com/SubmergedTree/smart-mirror-web-app" target="_blank">SMWA Repository</a></div>   
         <div><a href="https://github.com/SubmergedTree/SmartMirror" target="_blank">Smart Mirror Repository</a></div>   
