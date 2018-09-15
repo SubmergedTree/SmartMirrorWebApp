0c37c815a14c2f5de0f301580bd6b0376324b835
SubmergedTree <jannmann1@web.de>
2018-09-15 22:12:00 +0200

    window.opener is null in new window when target="_blank"

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,8 +5,8 @@ export default function Footer(props) {
     return (
     <div className="footer">
         <h3>Github</h3>
-        <div><a href="https://github.com/SubmergedTree/smart-mirror-web-app" target="_blank">SMWA Repository</a></div>   
-        <div><a href="https://github.com/SubmergedTree/SmartMirror" target="_blank">Smart Mirror Repository</a></div>   
+        <div><a href="https://github.com/SubmergedTree/smart-mirror-web-app" target="_blank" rel="noopener noreferrer">SMWA Repository</a></div>   
+        <div><a href="https://github.com/SubmergedTree/SmartMirror" target="_blank" rel="noopener noreferrer">Smart Mirror Repository</a></div>   
     </div>
     );
 }
